a8bc4e4bab86eef05ef404ef4ae57cbbc1f552ae
Shannon Pasto <shannonpasto@me.com>
2025-08-27 12:01:35 +1000

    Added voodoopad

---
+++ voodoopad.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+appInstallPath="/Applications"
+bundleName="VoodooPad"
+installedVers=$(/usr/bin/defaults read "${appInstallPath}"/"${bundleName}.app"/Contents/Info.plist CFBundleShortVersionString 2>/dev/null)
+
+currentVers=$(/usr/bin/curl -s "http://www.voodoopad.com" | /usr/bin/grep Version | /usr/bin/xmllint --xpath '//p/text()' - | /usr/bin/awk '{print $2}')
+downloadURL="https://voodoopad.s3.amazonaws.com/VoodooPad-${currentVers}.zip"
+FILE=${downloadURL##*/}
+
+# compare version numbers
+if [ "${installedVers}" ]; then
+  installedVersNoDots=$(/bin/echo "${installedVers}" | /usr/bin/sed 's/\.//g')
+  currentVersNoDots=$(/bin/echo "${currentVers}" | /usr/bin/sed 's/\.//g')
+
+  # pad out currentVersNoDots to match installedVersNoDots
+  installedVersNoDotsCount=${#installedVersNoDots}
+  currentVersNoDotsCount=${#currentVersNoDots}
+
+  while [ "${currentVersNoDotsCount}" -lt "${installedVersNoDotsCount}" ]; do
+    currentVersNoDots="${currentVersNoDots}0"
+    currentVersNoDotsCount=$((currentVersNoDotsCount + 1))
+  done
+
+  if [ "${installedVersNoDots}" -ge "${currentVersNoDots}" ]; then
+    /bin/echo "${bundleName} does not need to be updated"
+    exit 0
+  else
+    /bin/echo "${bundleName} needs to be updated"
+  fi
+else
+  /bin/echo "Installing ${bundleName}"
+fi
+
+if /usr/bin/curl --retry 3 --retry-delay 0 --retry-all-errors -sL "${downloadURL}" -o /tmp/"${FILE}"; then
+  /bin/rm -rf ${appInstallPath}/"${bundleName}.app" >/dev/null 2>&1
+  /usr/bin/ditto -xk /tmp/"${FILE}" "${appInstallPath}"/.
+  /usr/bin/xattr -r -d com.apple.quarantine "${appInstallPath}"/"${bundleName}.app"
+  /usr/sbin/chown -R root:admin "${appInstallPath}"/"${bundleName}.app"
+  /bin/chmod -R 755 "${appInstallPath}"/"${bundleName}.app"
+  /bin/rm /tmp/"${FILE}"
+fi
