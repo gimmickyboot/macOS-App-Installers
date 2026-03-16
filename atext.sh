547f8b4d4350c5cbbbd38e0eedd9e9b26bff9fbe
Shannon Pasto <shannonpasto@me.com>
2026-03-16 16:06:23 +1100

    added atext added obs_studio

---
+++ atext.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+appInstallPath="/Applications"
+bundleName="aText"
+appName="${bundleName}"
+installedVers=$(/usr/bin/defaults read "${appInstallPath}"/"${bundleName}.app"/Contents/Info.plist CFBundleShortVersionString 2>/dev/null)
+
+URL="https://www.trankynam.com/atext/"
+htmlData=$(/usr/bin/curl -s "${URL}" | /usr/bin/grep macOS | /usr/bin/xmllint --format --html -)
+currentVers=$(printf '%s' "${htmlData}" | /usr/bin/xmllint --html --xpath '//html/body/p[2]/text()' - | /usr/bin/awk '{print $2}')
+downloadURL="${URL}$(printf '%s' "${htmlData}" | /usr/bin/xmllint --html --xpath 'string(//a/@href)' -)"
+FILE=${downloadURL##*/}
+
+# compare version numbers
+if [ "${installedVers}" ]; then
+  /bin/echo "${appName} v${installedVers} is installed."
+  installedVersNoDots=$(/bin/echo "${installedVers}" | /usr/bin/sed 's/\.//g')
+  currentVersNoDots=$(/bin/echo "${currentVers}" | /usr/bin/sed 's/\.//g')
+
+  # pad out currentVersNoDots to match installedVersNoDots
+  installedVersNoDotsCount=${#installedVersNoDots}
+  currentVersNoDotsCount=${#currentVersNoDots}
+
+  while [ "${currentVersNoDotsCount}" -lt "${installedVersNoDotsCount}" ]; do
+    currentVersNoDots="${currentVersNoDots}0"
+    currentVersNoDotsCount=$((currentVersNoDotsCount + 1))
+  done
+
+  if [ "${installedVersNoDots}" -ge "${currentVersNoDots}" ]; then
+    /bin/echo "${appName} does not need to be updated"
+    exit 0
+  else
+    /bin/echo "Updating ${appName} to v${currentVers}"
+  fi
+else
+  /bin/echo "Installing ${appName} v${currentVers}"
+fi
+
+if /usr/bin/curl --retry 3 --retry-delay 0 --retry-all-errors -sL "${downloadURL}" -o /tmp/"${FILE}"; then
+  /bin/rm -rf "${appInstallPath}"/"${bundleName}.app" >/dev/null 2>&1
+  TMPDIR=$(mktemp -d)
+  /usr/bin/hdiutil convert -quiet /tmp/"${FILE}" -format UDTO -o /tmp/"${FILE}".cdr
+  /usr/bin/hdiutil attach /tmp/"${FILE}".cdr -noverify -quiet -nobrowse -mountpoint "${TMPDIR}"
+  /usr/bin/ditto "${TMPDIR}"/"${bundleName}.app" "${appInstallPath}"/"${bundleName}.app"
+  /usr/bin/xattr -r -d com.apple.quarantine "${appInstallPath}"/"${bundleName}.app"
+  /usr/sbin/chown -R root:admin "${appInstallPath}"/"${bundleName}.app"
+  /bin/chmod -R 755 "${appInstallPath}"/"${bundleName}.app"
+  /usr/bin/hdiutil eject "${TMPDIR}" -quiet
+  /bin/rmdir "${TMPDIR}"
+  /bin/rm /tmp/"${FILE}".cdr
+  /bin/rm /tmp/"${FILE}"
+fi
