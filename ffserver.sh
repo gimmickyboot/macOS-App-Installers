99e0b73e0bae603705cd7f95afbf8a303513f06c
Mac Guy <shannonpasto@me.com>
2026-08-26 13:38:02 +1000

    updated ffmpeg binaries

--- ffserver.sh
+++ ffserver.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
+# official ffmpeg binaries require rosetta
+
 appInstallPath="/usr/local/bin"
-bundleName="ffplay"
+bundleName="ffserver"
 appName="${bundleName}"
+if ! /usr/sbin/pkgutil --pkg-info=com.apple.pkg.RosettaUpdateAuto >/dev/null 2>&1; then
+  printf 'Rosetta 2 not installed. Can not continue\n'
+  exit 1
+fi
 installedVers=$("${appInstallPath}"/"${bundleName}" -version | awk '{print $3}' | head -n 1 | awk -F "-" '{print $1}')
 
 jsonData=$(/usr/bin/curl -s "https://evermeet.cx/ffmpeg/info/ffserver/release")
 if [ "$(/usr/bin/sw_vers -buildVersion | /usr/bin/cut -c 1-2 -)" -ge 22 ]; then
-  currentVers=$(printf '%s' "${jsonData}" | jq -r .version)
-  downloadURL=$(printf '%s' "${jsonData}" | jq -r .download.zip.url)
+  currentVers=$(printf '%s' "${jsonData}" | /usr/bin/jq -r .version)
+  downloadURL=$(printf '%s' "${jsonData}" | /usr/bin/jq -r .download.zip.url)
 else
   currentVers=$(printf '%s' "${jsonData}" | /usr/bin/plutil -extract version raw -o - -)
   downloadURL=$(printf '%s' "${jsonData}" | /usr/bin/plutil -extract download.zip.url raw -o - -)
